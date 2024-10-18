4ea563adce912c6ba6325e0b1783be4e64525eb7
pyflon <115070724+pyflon@users.noreply.github.com>
2024-10-18 14:39:24 +0200

    Anpassung Enum
    Integer ausgelassen

--- Assignment01/CODE/Produkt.java
+++ Assignment01/CODE/Produkt.java
@@ -1,84 +1,49 @@
-
 /**
- * Write a description of class Produkt here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * Represents a product with various states.
  */
-
-class Produkt 
-{
-    //eine globale Variabel wird gesetzt, damit die anderen Klassen auch zugriff drauf haben
+class Produkt {
+    // A global variable to hold the product's state
     private Zustand zustand;
-    
-    //Enum: mögliche Zustände des Produkts wird vordefiniert, Enumeratuin der Klasse bestellZustand 
-    enum Zustand { 
-        Bestellt, //1
-        In_Produktion, //2
-        Geliefert //3
+
+    // Enum: Possible states of the product
+    enum Zustand {
+        Bestellt,       // Ordered
+        In_Produktion,  // In Production
+        Geliefert       // Delivered
     }
 
-    // Konstruktor 
+    // Constructor
     public Produkt() {
-        this.zustand = Zustand.Bestellt; // Anfangszustand definieren
+        this.zustand = Zustand.Bestellt; // Define the initial state
     }
-    
-    public Zustand getZustand(){
-        return this.zustand; //Zustand zurückgeben
+
+    // Get the current state of the product
+    public Zustand getZustand() {
+        return this.zustand; // Return the current state
     }
-    
-    public int getIntZustand(){
-        if(this.zustand == Zustand.Bestellt) {
-            return (1); //Bestellt
-        }
-        else if (this.zustand == Zustand.In_Produktion){
-            return (2); //In_Produktion
-        }
-        else if (this.zustand == Zustand.Geliefert){
-            return (3); //Geliefert
-        }
-        else {
-            System.out.println("ACHTUNG: Es wurde kein gültiger Zustand gefunden");
-            return (0); 
-        }
-    }
-    
-  // Method to change the state of the product
+
+    // Set the current state of the product
     public void setZustand(Zustand aktuellerZustand) {
         this.zustand = aktuellerZustand; // Update the product's state
     }
-    
-    // Methode um Zustand eines Produkts festzulegen (int)
-    public void setIntZustand(int aktuellerZustand){
-        if (aktuellerZustand == 1) {
-            setZustand(Zustand.Bestellt);
-        }
-        if (aktuellerZustand == 2) {
-            setZustand(Zustand.In_Produktion);
-        }
-        if (aktuellerZustand == 3) {
-            setZustand(Zustand.Geliefert);
-        }
-        else {
-            System.out.println("Ungültiger Wert: Zustand bleibt unverändert. Erhaltener Wert:" + aktuellerZustand);
-        }
-    }   
-    // Methode um den aktuellen Zustand des Produkts zu erhalten
-    public int gibZustand(){
-        switch (zustand) {
+
+    // Get the integer representation of the current state
+    public int getIntZustand() {
+        switch (this.zustand) {
             case Bestellt:
-                return 1;
+                return 1; // Bestellt
             case In_Produktion:
-                return 2; 
+                return 2; // In Produktion
             case Geliefert:
-                return 3; 
+                return 3; // Geliefert
             default:
-                return 0; //Falls ein unbekannter Zustand vorliegt
-            }
+                System.out.println("ACHTUNG: Es wurde kein gültiger Zustand gefunden");
+                return 0; // If an unknown state is encountered
         }
     }
- 
-        
 
-
-
+    // Method to provide the current state as an integer
+    public int gibZustand() {
+        return getIntZustand(); // Reuse the existing integer representation method
+    }
+}
